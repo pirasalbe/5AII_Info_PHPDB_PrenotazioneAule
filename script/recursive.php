f22f88ff8e1616fbd3bdd1321ebd00adcfac82c8
pirasalbe <alberto98.piras@gmail.com>
2017-04-13 11:11:37 +0200

    Added https (without ca)

--- script/recursive.php
+++ script/recursive.php
@@ -1,26 +1,32 @@
 <?php
-	session_start();
-	
-	$logged = 0;
-	if(isset($_SESSION['user']))
-		$logged='1';
-	
-	function printNavbar($admin){
-		global $logged;
-		
-		if($admin)
-			$admin = "../";
-		else 
-			$admin = "";
-		
-		$user = "Area Riservata";
-		$log = "Login";
-		if($logged == 1){
-			$log = "Logout";
-			$user = $_SESSION['user'];
-		}
-		
-		echo "<!--- header --->
+session_start();
+
+/*if ($_SERVER["HTTPS"] != "on") {
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+}*/
+
+$logged = 0;
+if (isset($_SESSION['user']))
+    $logged = '1';
+
+function printNavbar($admin)
+{
+    global $logged;
+
+    if ($admin)
+        $admin = "../";
+    else
+        $admin = "";
+
+    $user = "Area Riservata";
+    $log = "Login";
+    if ($logged == 1) {
+        $log = "Logout";
+        $user = $_SESSION['user'];
+    }
+
+    echo "<!--- header --->
 			<nav class='navbar navbar-default'>
 				<div class='container-fluid'>
 					<div class='navbar-header'>
@@ -42,16 +48,17 @@
 					</ul>
 				</div>
 			</nav>";
-	}
-	
+}
 
-	function printHeader(){
-		echo "<meta charset='utf-8'>
+
+function printHeader()
+{
+    echo "<meta charset='utf-8'>
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
 		<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
 		<link rel='stylesheet' href='style/font-awesome/css/font-awesome.min.css'>
 		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js'></script>
 		<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>";
-	}	
-	
+}
+
 ?>
