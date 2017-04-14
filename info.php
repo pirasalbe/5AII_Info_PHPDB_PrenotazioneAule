166b0569d2dd22a001020c64c12cf80a40f4a387
pirasalbe <alberto98.piras@gmail.com>
2017-04-14 10:56:39 +0200

    Informazioni su prenotazione

--- info.php
+++ info.php
@@ -1,15 +1,20 @@
 <?php
 include("script/recursive.php");
 
-if ($logged == 0) {
-    header("location: login");
-}
+//info?utente=" . $book['utente'] . "&aula=" . $book['aula'] . "&dettagli=" . $book['dettagli'] . "&inizio=" . $book['inizio'] . "&fine=" . $book['fine'] . "'
+
+$utente = $_REQUEST['utente'];
+$aula = $_REQUEST['aula'];
+$dettagli = $_REQUEST['dettagli'];
+$inizio = $_REQUEST['inizio'];
+$fine = $_REQUEST['fine'];
+
 ?>
 
 <html>
 <head>
     <title>
-        Prenota
+        Informazioni
     </title>
     <?php printHeader(); ?>
 </head>
@@ -24,90 +29,72 @@ if ($logged == 0) {
 
 <!--- create booking --->
 <div class="container">
-    <h1>Prenotati</h1><br>
-    <form action="script/book">
+    <h1>Prenotazione</h1><br>
 
-        <div class="row form-group">
-            <div class="col-sm-2">
-                <label for="aula">Aula: </label>
-            </div>
-            <div class="col-sm-4">
-                <select name="aula" class="form-control">
-                    <?php
-
-                    $result = rooms();
-
-                    if (isset($result) && $result != null) {
-                        while ($row = $result->fetch_assoc()) {
-                            $selected = "";
-                            if ($row['numero'] == $_REQUEST['aula']) $selected = "selected";
-                            echo "<option value='" . $row['numero'] . "' " . $selected . ">" . $row["nome"] . " Tipo: " . $row["type"] . "</option>";
-                        }
-                    }
-
-                    ?>
-                </select>
-            </div>
-            <div class="col-sm-2">
-                <label for="date">Data: </label>
-            </div>
-            <div class="col-sm-4">
-                <input type="date" value="<?php echo date("Y-m-d", strtotime($_REQUEST['data'])); ?>" name="date" class="form-control">
-            </div>
+    <div class="row form-group">
+        <div class="col-sm-2">
+            <label for="aula">Aula: </label>
         </div>
+        <div class="col-sm-4">
+            <?php
 
-        <div class="row form-group">
-            <div class="col-sm-2">
-                <label for="descrizione">Descrizione: </label>
-            </div>
-            <div class="col-sm-10">
-                <input name="descrizione" class="form-control" maxlength="50">
-            </div>
+            $result = rooms();
+
+            if (isset($result) && $result != null) {
+                while ($row = $result->fetch_assoc()) {
+                    if ($row['numero'] == $aula) echo $row["nome"] . " Tipo: " . $row["type"];
+                }
+            }
+
+            ?>
         </div>
-
-        <div class="row form-group">
-            <div class="col-sm-2">
-                <label for="orai">Ora inizio: </label>
-            </div>
-            <div class="col-sm-4">
-                <select name="orai" class="form-control">
-                    <?php
-
-                    foreach ($hours as $hour) {
-                        $selected = "";
-                        if ($hour == $_REQUEST['inizio']) $selected = "selected";
-                        echo "<option value='" . $hour . "' " . $selected . ">" . $hour . "</option>";
-                    }
-
-                    ?>
-                </select>
-            </div>
-            <div class="col-sm-2">
-                <label for="oraf">Ora fine: </label>
-            </div>
-            <div class="col-sm-4">
-                <select name="oraf" class="form-control">
-                    <?php
-
-                    foreach ($hours as $hour) {
-                        $selected = "";
-                        if ($hour == $_REQUEST['fine']) $selected = "selected";
-                        echo "<option value='" . $hour . "' " . $selected . ">" . $hour . "</option>";
-                    }
-
-                    ?>
-                </select>
-            </div>
+        <div class="col-sm-2">
+            <label for="date">Data: </label>
         </div>
-
-        <div class="row form-group">
-            <div class="col-sm-2">
-                <button type="submit" class="form-control">
-                    Richiedi
-                </button>
-            </div>
+        <div class="col-sm-4">
+            <?php echo date("Y-m-d", strtotime($_REQUEST['inizio'])); ?>
         </div>
-    </form>
+    </div>
+
+    <div class="row form-group">
+        <div class="col-sm-2">
+            <label for="descrizione">Descrizione: </label>
+        </div>
+        <div class="col-sm-4">
+            <?php echo $dettagli; ?>
+        </div>
+        <div class="col-sm-2">
+            <label for="descrizione">Creatore: </label>
+        </div>
+        <div class="col-sm-4">
+            <?php
+
+            $result = users();
+
+            if (isset($result) && $result != null) {
+                while ($row = $result->fetch_assoc()) {
+                    if ($row['username'] == $utente) echo $row['nome'];
+                }
+            }
+
+            ?>
+        </div>
+    </div>
+
+    <div class="row form-group">
+        <div class="col-sm-2">
+            <label for="orai">Ora inizio: </label>
+        </div>
+        <div class="col-sm-4">
+            <?php echo date("H:i", strtotime($_REQUEST['inizio'])); ?>
+        </div>
+        <div class="col-sm-2">
+            <label for="oraf">Ora fine: </label>
+        </div>
+        <div class="col-sm-4">
+            <?php echo date("H:i", strtotime($_REQUEST['fine'])); ?>
+        </div>
+    </div>
 </div>
 
 </body>
